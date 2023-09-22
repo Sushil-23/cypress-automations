113674a1ded418d5cae9f87939c42b42b768ce34
Sushil-23 <kamblesushil367@gmail.com>
2023-09-22 23:44:18 +0530

    Added selector file and assertions

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -3,13 +3,16 @@
 describe("Test contact us form via webdriver uni", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+        cy.title().should('eq', 'WebDriver | Contact Us')
+        cy.url().should('include', 'Contact-Us/contactus')
         //cy.get('#contact-us').click({force: true})
         cy.get('[name="first_name"]').type("Joe");
         cy.get('[name="last_name"]').type("blogs");
         cy.get('[name="email"]').type("joe_blogs222@gmail.com")
         cy.get('textarea.feedback-input').type("How can I learn cypress?")
         cy.get('[type="submit"]').click()
-
+        cy.get('h1').should('have.text','Thank You for your Message!');
         cy.get('h1').then(function($successMessage)
         {
             const successMessage=$successMessage.text()
@@ -24,6 +27,7 @@ describe("Test contact us form via webdriver uni", () => {
         cy.get('textarea.feedback-input').type("How can I learn cypress?")
         cy.get('[type="submit"]').click()
 
+        cy.get('body').contains('Error: Invalid email address');
         cy.get('body').then(function($errorMessage)
         {
             const errorMessage=$errorMessage.text()
