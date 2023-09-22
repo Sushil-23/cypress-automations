d203d88a28c53a3d22887e57664e73f27265f99f
Sushil-23 <kamblesushil367@gmail.com>
2023-09-22 23:07:16 +0530

    Improved selector for contact us page

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -3,11 +3,11 @@
 describe("Test contact us form via automation test store", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://automationteststore.com/");
-        cy.get("a[href*='content/contact']").click();
+        cy.get("a[href$='content/contact']").click();
         cy.get('#ContactUsFrm_first_name').type("Joe");
         cy.get('#ContactUsFrm_email').type("joe_blogs555@gmail.com");
         cy.get('#ContactUsFrm_enquiry').type("Do you  provide additional discount on bulk orders?");
-        cy.get('.col-md-6 > .btn').click();
+        cy.get('button[title="Submit"]').click();
 
         cy.get('.mb40 > :nth-child(3)').then(function($successMessage)
         {
