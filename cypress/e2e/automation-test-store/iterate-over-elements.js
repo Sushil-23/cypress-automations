916e23898ece8998b4b69c063987cd67fab1f709
Sushil-23 <kamblesushil367@gmail.com>
2023-09-23 18:12:12 +0530

    Modified iterate over elements

--- cypress/e2e/automation-test-store/iterate-over-elements.js
+++ cypress/e2e/automation-test-store/iterate-over-elements.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe("Iterate over elements", () => {
-    it.only("Log information of all hair care products", () => {
+    it("Log information of all hair care products", () => {
         cy.visit("https://automationteststore.com/");
         cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
         cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
@@ -12,7 +12,12 @@ describe("Iterate over elements", () => {
     it("Add specific product to basket", () => {
         cy.visit("https://automationteststore.com/");
         cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
-
+        cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
+            if($el.text().includes('Curls to straight Shampoo'))
+            {
+                cy.wrap($el).click()
+            }
+        })
     })
 
 })
