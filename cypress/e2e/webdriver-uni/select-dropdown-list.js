3c0d5c4f3289e2052a18287d84cdbffca501146d
Sushil-23 <kamblesushil367@gmail.com>
2023-09-24 22:49:22 +0530

    Added dropwdown spec

---
+++ cypress/e2e/webdriver-uni/select-dropdown-list.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+describe("Interate with drop down list via webdriveruni", () => {
+    it("Select specific value via dropdown list", () => {
+        cy.visit("https://webdriveruniversity.com/");
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click({force:true});
+       
+        cy.get('#dropdowm-menu-1').select('c#');
+        cy.get('#dropdowm-menu-2').select('TestNG');
+        cy.get('#dropdowm-menu-3').select('jquery');
+        cy.get('#dropdowm-menu-3').select('JavaScript').should('have.value','javascript');
+
+        //challenge
+        cy.get('#dropdowm-menu-2').select('maven').contains('Maven');
+        cy.get('#dropdowm-menu-2').select('TestNG').should('have.value','testng');
+
+
+
+    })
+})
