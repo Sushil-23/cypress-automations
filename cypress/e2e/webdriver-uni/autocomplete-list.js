3ebbaabaae0672b9e01002f925c2fcffd34f9f6f
Sushil-23 <kamblesushil367@gmail.com>
2023-09-24 23:14:02 +0530

    Added auto complete list spec

---
+++ cypress/e2e/webdriver-uni/autocomplete-list.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+describe("Verify autocomplete dropdown list via webdriveruni", () => {
+    it("Select specific product via auto complete list", () => {
+        cy.visit("https://webdriveruniversity.com/");
+        cy.get('#autocomplete-textfield').invoke('removeAttr', 'target').click({ force: true });
+        cy.get('#myInput').type('A');
+        cy.get('.autocomplete-items > *').each(($el, index, $list) => {
+            const prod = $el.text();
+            const productToSelect = "Avacado";
+            if (prod === productToSelect) {
+                cy.wrap($el).click()
+                cy.get('#myInput').should('have.value', 'Avacado')
+                cy.get('#submit-button').click()
+                cy.url().should('include', 'food-item=Avacado')
+            }
+        })
+
+    })
+})
