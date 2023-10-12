6cf0c523c3163c7292f6eeb2d0d5796fdd0f5cd0
Sushil-23 <kamblesushil367@gmail.com>
2023-10-12 22:21:14 +0530

    Added hooks and modified contact us automation test store spec

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -1,13 +1,21 @@
 /// <reference types="Cypress" />
 
 describe("Test contact us form via automation test store", () => {
+
+    before(function() {
+        cy.fixture('userDetails').as('user')
+    })
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://automationteststore.com/");
         cy.get("a[href$='content/contact']").click().then(function(linkText) {
             cy.log("Clicked on the link using text : "+linkText.text())
         });
-        cy.get('#ContactUsFrm_first_name').type("Joe");
-        cy.get('#ContactUsFrm_email').type("joe_blogs555@gmail.com");
+
+        cy.get('@user').then((user) => {
+            cy.get('#ContactUsFrm_first_name').type(user.first_name);
+            cy.get('#ContactUsFrm_email').type(user.email);
+        })
+        
         cy.get('#ContactUsFrm_email').should('have.attr','name','email');
         cy.get('#ContactUsFrm_enquiry').type("Do you  provide additional discount on bulk orders?");
         cy.get('button[title="Submit"]').click();
