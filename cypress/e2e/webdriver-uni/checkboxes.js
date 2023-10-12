125513e3a70954c271d12ca0b821dfe5bc65438e
Sushil-23 <kamblesushil367@gmail.com>
2023-10-12 20:43:51 +0530

    Modified and added hooks for checkboxes spec

--- cypress/e2e/webdriver-uni/checkboxes.js
+++ cypress/e2e/webdriver-uni/checkboxes.js
@@ -1,23 +1,21 @@
 /// <reference types="Cypress" />
 
 describe("Verifying checkboxes via webdriveruni", () => {
-    it("Check and validate checkbox", () => {
+    beforeEach(() => {
         cy.visit("https://webdriveruniversity.com/");
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click({force:true});
+    })
+    it("Check and validate checkbox", () => {
         cy.get('[value="option-1"]').as('option-1')
         cy.get('@option-1').check().should('be.checked');
     })
 
     it("Uncheck and validate checkbox", () => {
-        cy.visit("https://webdriveruniversity.com/");
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click({force:true});
         cy.get('[value="option-3"]').as('option-3')
         cy.get('@option-3').uncheck().should('not.be.checked');
     })
 
     it("Check and validate multiple checkbox", () => {
-        cy.visit("https://webdriveruniversity.com/");
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click({force:true});
         cy.get('[type="checkbox"]').as('checkbox')
         cy.get('@checkbox').check(['option-1','option-2','option-3','option-4']).should('be.checked');
         cy.get('@checkbox').uncheck(['option-1','option-2']).should('not.be.checked');
