69b8ec8bf221ceb80178e12b2bab7c2ab835523b
Sushil-23 <kamblesushil367@gmail.com>
2023-10-12 22:08:06 +0530

    Used fixture to drive data and accordingly modififed contact-us spec

--- cypress/e2e/webdriver-uni/contact-us.js
+++ cypress/e2e/webdriver-uni/contact-us.js
@@ -1,16 +1,21 @@
 /// <reference types="Cypress" />
 
 describe("Test contact us form via webdriver uni", () => {
+    before(function() {
+        cy.fixture('example').then(function(data) {
+            globalThis.data = data;
+        })
+    })
     it("Should be able to submit a successful submission via contact us form", () => {
-        cy.visit("https://webdriveruniversity.com/")
+        cy.visit("http://webdriveruniversity.com/")
         cy.get('#contact-us').invoke('removeAttr','target').click({force:true})
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
         cy.title().should('eq', 'WebDriver | Contact Us')
         cy.url().should('include', 'Contact-Us/contactus')
         //cy.get('#contact-us').click({force: true})
-        cy.get('[name="first_name"]').type("Joe");
-        cy.get('[name="last_name"]').type("blogs");
-        cy.get('[name="email"]').type("joe_blogs222@gmail.com")
+        cy.get('[name="first_name"]').type(data.first_name);
+        cy.get('[name="last_name"]').type(data.last_name);
+        cy.get('[name="email"]').type(data.email)
         cy.get('textarea.feedback-input').type("How can I learn cypress?")
         cy.get('[type="submit"]').click()
         cy.get('h1').should('have.text','Thank You for your Message!');
@@ -22,10 +27,10 @@ describe("Test contact us form via webdriver uni", () => {
     })
 
     it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
-        cy.visit("https://webdriveruniversity.com/")
+        cy.visit("http://webdriveruniversity.com/")
         cy.get('#contact-us').invoke('removeAttr','target').click({force:true})
-        cy.get('[name="first_name"]').type("Tom");
-        cy.get('[name="last_name"]').type("blogs");
+        cy.get('[name="first_name"]').type(data.first_name);
+        cy.get('[name="last_name"]').type(data.last_name);
         cy.get('textarea.feedback-input').type("How can I learn cypress?")
         cy.get('[type="submit"]').click()
 
