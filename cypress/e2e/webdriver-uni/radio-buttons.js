24f78b334c899d5f5920b7387ce0c414389eefb7
Sushil-23 <kamblesushil367@gmail.com>
2023-10-12 20:38:27 +0530

    Added hooks in radio-buttons spec

--- cypress/e2e/webdriver-uni/radio-buttons.js
+++ cypress/e2e/webdriver-uni/radio-buttons.js
@@ -1,18 +1,18 @@
 /// <reference types="Cypress" />
 
 describe("Verifying radio buttons via webdriveruni", () => {
-    it("Check specific radio button", () => {
+    beforeEach(function() {
         cy.visit("https://webdriveruniversity.com/");
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click({force:true});
+        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force: true });
+    })
+    it("Check specific radio button", () => {
         cy.get('#radio-buttons').find('[type="radio"]').as('radio-button');
         cy.get('@radio-button').first().check().should('be.checked')
-        cy.get('@radio-button').eq(1).check().should('be.checked').and('have.value','blue')
+        cy.get('@radio-button').eq(1).check().should('be.checked').and('have.value', 'blue')
         cy.get('@radio-button').first().should('not.be.checked')
     })
 
     it("Validate state of specific radio button", () => {
-        cy.visit("https://webdriveruniversity.com/");
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr','target').click({force:true});
         cy.get('[value="lettuce"]').should('not.be.checked')
         cy.get('[value="pumpkin"]').should('be.checked')
         cy.get('[value="lettuce"]').check().should('be.checked')
