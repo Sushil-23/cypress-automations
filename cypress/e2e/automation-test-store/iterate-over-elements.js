84def6a5e218e5e1d02ef3896b1ced973c21aefe
Sushil-23 <kamblesushil367@gmail.com>
2023-10-31 21:12:18 +0530

    Added beforeEach hook in iterate over element spec

--- cypress/e2e/automation-test-store/iterate-over-elements.js
+++ cypress/e2e/automation-test-store/iterate-over-elements.js
@@ -1,17 +1,18 @@
 /// <reference types="Cypress" />
 
 describe("Iterate over elements", () => {
-    it("Log information of all hair care products", () => {
+
+    beforeEach(() => {
         cy.visit("https://automationteststore.com/");
         cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
+    })
+    it("Log information of all hair care products", () => {
         cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
             cy.log("Index: " + index + " : " + $el.text())
         })
     })
 
     it("Add specific product to basket", () => {
-        cy.visit("https://automationteststore.com/");
-        cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
         // cy.get('.fixed_wrapper .prdocutname').each(($el, index, $list) => {
         //     if($el.text().includes('Curls to straight Shampoo'))
         //     {
@@ -23,15 +24,10 @@ describe("Iterate over elements", () => {
     })
 
     it("Add another specific product to basket", () => {
-        cy.visit("https://automationteststore.com/");
-        cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
         cy.selectProduct('Seaweed Conditioner');
-
     })
 
     it("Add one more specific product to basket", () => {
-        cy.visit("https://automationteststore.com/");
-        cy.get("a[href*='product/category&path=']").contains('Hair Care').click();
         cy.selectProduct('Pantene Pro-V Conditioner, Classic Care');
 
     })
